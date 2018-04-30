a12e22f66dc25f94200f297a0ca8fcb56836a8a3
Christoph Kurrat <christoph.kurrat@andrena.de>
2018-04-30 10:30:18 +0200

    init

---
+++ src/main/java/com/kurromat/hikingmetrics/HikingmetricsApplication.java
@@ -0,0 +1,12 @@
+package com.kurromat.hikingmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingmetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikingmetricsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kurromat/hikingmetrics/HikingmetricsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kurromat.hikingmetrics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HikingmetricsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
